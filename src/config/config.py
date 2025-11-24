353a4c5ba3acbf6ee840f8cb7eae425f28100607
developer-risiga <developer@risiga.com>
2025-11-24 17:01:09 +0530

    Updated the config.py

---
+++ src/config/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    def __init__(self):
+        self.APP_NAME = "Butler Voice Assistant"
+        self.VERSION = "1.0.0"
+        self.DEBUG = True
+        self.DEFAULT_LOCATION = "Bangalore"
+
+print("Config class defined")
